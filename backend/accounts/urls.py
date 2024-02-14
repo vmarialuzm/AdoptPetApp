853b8c545a4ea41931318edfa78684c92b54609d
vmarialuz <vmarialuzm@gmail.com>
2024-02-14 13:11:52 -0500

    api users

---
+++ backend/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='users'),
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
