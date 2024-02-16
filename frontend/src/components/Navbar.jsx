3cde1bb79cf4e6710451bf9c20efe088b746b97a
vmarialuz <vmarialuzm@gmail.com>
2024-02-16 12:48:25 -0500

    add form login

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,52 +3,17 @@ import { Link } from "react-router-dom"
 export const Navbar = () => {
   return (
     <>
-        <header className="bg-amber-400">
-            <div className="flex h-16 items-center justify-between">
-                <button className="text-slate-500 hover:text-slate-900">
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        fill="none" 
-                        viewBox="0 0 24 24" 
-                        stroke-width="1.5" 
-                        stroke="currentColor" 
-                        class="w-6 h-6">
-                        <path 
-                            stroke-linecap="round" 
-                            stroke-linejoin="round" 
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
 
-                </button>
-
-                <button>
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        class="icon icon-tabler icon-tabler-cat" 
-                        width="24" 
-                        height="24" 
-                        viewBox="0 0 24 24" 
-                        stroke-width="1.5" 
-                        stroke="currentColor" 
-                        fill="none" 
-                        stroke-linecap="round" 
-                        stroke-linejoin="round">
-                        <path 
-                            stroke="none" 
-                            d="M0 0h24v24H0z" 
-                            fill="none"/>
-                        <path 
-                            d="M20 3v10a8 8 0 1 1 -16 0v-10l3.432 3.432a7.963 7.963 0 0 1 4.568 -1.432c1.769 0 3.403 .574 4.728 1.546l3.272 -3.546z" /><path d="M2 16h5l-4 4" /><path d="M22 16h-5l4 4" /><path d="M12 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M9 11v.01" /><path d="M15 11v.01" /></svg>
-                </button>
-
+        <div className="flex py-3 items-center justify-between">
+            <div>
+                AdoptPetApp
+            </div>
+            <div className="space-x-4">
                 <Link to='/signin'>signin</Link>
                 <Link to='/login'>login</Link>
-                <div>
-                    <button>theme</button>
-                    <button>user</button>
-                </div>
             </div>
-        </header>
+        </div>
+
     </>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,24 @@
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className="max-w-xl mx-auto">
+        <form action="">
+            <input 
+                type="text" 
+                placeholder="email" 
+                className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+            />
+
+            <input 
+                type="password" 
+                placeholder="password" 
+                className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+            />
+            <button 
+                type="submit"
+                className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+                Save
+            </button>
+        </form>
+    </div>
   )
 }
