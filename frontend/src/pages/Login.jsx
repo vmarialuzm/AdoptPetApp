e37f5a60bc4af66d2d126f535ee08e74e2c898b5
vmarialuz <vmarialuzm@gmail.com>
2024-02-16 12:53:38 -0500

    signin form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 export const Login = () => {
   return (
     <div className="max-w-xl mx-auto">
+        <h1>Login</h1>
         <form action="">
             <input 
                 type="text" 
@@ -13,10 +14,11 @@ export const Login = () => {
                 placeholder="password" 
                 className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
             />
+            
             <button 
                 type="submit"
                 className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
-                Save
+                Iniciar Sesión
             </button>
         </form>
     </div>
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,6 +1,33 @@
 export const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className="max-w-xl mx-auto">
+        <h1>Signin</h1>
+        <form action="">
+            <input 
+                type="email" 
+                placeholder="email" 
+                className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+            />
+
+            <input 
+                type="username" 
+                placeholder="username" 
+                className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+            />
+
+            <input 
+                type="password" 
+                placeholder="password" 
+                className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+            />
+            
+            <button 
+                type="submit"
+                className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+                Save
+            </button>
+        </form>
+    </div>
   )
 }
 
