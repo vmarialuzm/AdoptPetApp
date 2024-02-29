ce59f308eca366aaf0e7d8cbd650f772c4927d33
vmarialuz <vmarialuzm@gmail.com>
2024-02-28 21:13:32 -0500

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,17 +3,30 @@ import { Link } from "react-router-dom"
 export const Navbar = () => {
   return (
     <>
-
-        <div className="flex py-3 items-center justify-between">
+      <nav className="bg-white py-6 relative">
+        <div className="container mx-auto flex">
+          <div className="flex flex-grow">
+            <img src="" alt="AdoptPetApp" />
+          </div>
+          
+          <div className="flex flex-grow justify-between">
             <div>
-                AdoptPetApp
+              <Link className="lg:mr-7">Adopta</Link>
+              <Link className="lg:mr-7">Club</Link>
+              <Link className="lg:mr-7">Consultas</Link>
+              <Link className="lg:mr-7">Blog</Link>
+              <Link className="lg:mr-7">Nosotros</Link>
+              <Link>Perros Perdidos</Link>
             </div>
-            <div className="space-x-4">
-                <Link to='/signin'>signin</Link>
-                <Link to='/login'>login</Link>
-            </div>
-        </div>
 
+            <div className="">
+              <Link to='/signin' className="bg-js py-2.5 px-5 rounded-full lg:mr-4">Registrate</Link>
+              <Link to='/login' className="text-yellow-400">Iniciar sesión</Link>
+            </div>
+          </div>
+
+        </div>
+      </nav>
     </>
   )
 }
