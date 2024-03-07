30b93c90b8ce10180d3e8ab43cb4127296bab4d8
vmarialuz <vmarialuzm@gmail.com>
2024-03-07 17:31:29 -0500

    modified views Blog

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,11 +1,8 @@
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import path
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'blog', views.PetViewSet, basename='blog'),
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('list/', views.BlogListViewSet.as_view()),
 
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,9 +1,17 @@
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from rest_framework import status
 from .models import Blog
 from .serializers import BlogSerializer
 
-class PetViewSet(viewsets.ModelViewSet):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
+class BlogListViewSet(APIView):
     permission_classes = [AllowAny]
+    def get(self, request, format=None):
+        if Blog.objects.all().exists():
+            blogs = Blog.objects.all()
+            serializer = BlogSerializer(blogs, many=True)
+
+            return Response({'blogs': serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'No blogs found'}, status=status.HTTP_404_NOT_FOUND)
