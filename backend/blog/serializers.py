b2b5a6bcdfc2461b82e0b8bc2b4154cb1d761780
vmarialuz <vmarialuzm@gmail.com>
2024-03-07 16:46:32 -0500

    mostrar los datos de user

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Blog
+from accounts.serializers import CustomUserSerializer
 
 class BlogSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
     class Meta:
         model = Blog
-        fields = '__all__'
+        fields = [
+            'id',
+            'user',
+            'image',
+            'date',
+            'minutes',
+            'title',
+            'content'
+        ]
